457bf8ba3dbbc9b79a6ddf4e8e3761117604d55c
donCelis <alexcelis019@gmail.com>
2024-03-05 21:21:26 -0500

    repaso condicionales

---
+++ repaso/app.js
@@ -0,0 +1,38 @@
+/* 
+valores de verdad
+tabla de verdad
+
+
+AND (&&) -> esto devuelve true si todas son true
+OR (||) -> esto devuelve false si todas son false
+NOT (!) -> negar un valor de verdad
+*/
+
+/* const test = 10 * "c";
+console.log(test);
+console.log(typeof test); */
+
+/* Solicitar al usuario que responda a la pregunta (“¿Eres bellisimo/a?”), 
+en caso de contestar sí, responder “Ciertamente”, en caso de contestar no, 
+responder: “No te creo”. */
+
+/* 
+  1 - datos de entrada
+  2 - operaciones
+  3 - datos de salida
+*/
+
+// const pregunta = prompt("¿Eres bellisimo/a?");
+const pregunta = 'si';
+const sanitize = pregunta.trim().toLocaleLowerCase();
+
+const isAfirmative = "si";
+let response = "";
+
+if (sanitize === isAfirmative) {
+  response = "Ciertamente";
+} else {
+  response = "No te creo";
+}
+
+console.log(response);
